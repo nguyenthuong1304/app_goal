88203b98e0fa4854c8ae04f9f8090c35aa60431c
Nguyen Thuong <nguyen.ngoc.thuong@sun-asterisk.com>
2021-03-01 16:00:13 +0700

    Setup docker-compose

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -9,7 +9,6 @@ use App\Models\User;
 use App\Http\Requests\ArticleRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redis;
 
 class ArticleController extends Controller
 {
